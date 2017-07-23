40aa572d4296cb6f4bf3e56637382086f83ce098
Carlos Vieira <cae.vieira@gmail.com>
2017-07-23 21:30:21 +0100

    Create feature file

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -1,8 +1,10 @@
 <?php
 
 use Behat\Behat\Context\Context;
+use Behat\Behat\Tester\Exception\PendingException;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
+use Money\Money;
 
 /**
  * Defines application features from the specific context.
@@ -18,5 +20,86 @@ class FeatureContext implements Context
      */
     public function __construct()
     {
+    }
+
+    /**
+     * @Given :investor has :amount in his virtual wallet
+     */
+    public function hasInHisVirtualWallet(Investor $investor, Money $amount)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Given a loan starts on :loanBeginDate and ends on :loanEndDate
+     */
+    public function aLoanStartsOnAndEndsOn(\DateTimeImmutable $loanBeginDate, \DateTimeImmutable $loanEndDate)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Given the loan has the following tranches:
+     */
+    public function theLoanHasTheFollowingTranches(TableNode $table)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Given :investor invest :arg2 on tranche :tranche on :investDate
+     * @Given :investor invest :arg2 on tranche :tranche
+     */
+    public function investOnTrancheAOn(Investor $investor, Tranche $tranche, DateTimeImmutable $investDate = null)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @When the interest is calculated for the period of :startDate to :endDate
+     */
+    public function theInterestCalculationIsDoneOnForThePeriodOfTo(DateTimeImmutable $startDate, DateTimeImmutable $endDate)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then :investor earns :amount
+     */
+    public function earns(Investor $investor, Money $amount)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Given a loan has the following tranches:
+     */
+    public function aLoanHasTheFollowingTranches(TableNode $table)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Given investors have already invested :amount on tranche :tranche
+     */
+    public function investorsHaveAlreadyInvestedOnTrancheA(Money $amount, Tranche $tranche)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then the investor should get an exception error message
+     */
+    public function theInvestorShouldGetAnExceptionErrorMessage()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Given investors have already invested :amount on :tranche
+     */
+    public function investorsHaveAlreadyInvestedOnTranche(Money $amount, Tranche $tranche)
+    {
+        throw new PendingException();
     }
 }
--- features/getting_a_loan.feature
+++ features/getting_a_loan.feature
@@ -11,17 +11,17 @@ Feature: Getting a loan
   - At the end of the month we need to calculate the interest each investor is due to be paid.
 
   Background:
-    Given "investor 1" has 1000 GBP in his virtual wallet
-    And "investor 2" has 1000 GBP in his virtual wallet
+    Given "investor 1" has "1000 GBP" in his virtual wallet
+    And "investor 2" has "1000 GBP" in his virtual wallet
 
   Scenario:
-    Given a loan starts on 01/10/2015 and ends on 15/11/2015
+    Given a loan starts on "01/10/2015" and ends on "15/11/2015"
     And the loan has the following tranches:
       | Name | Interest | Amount Available |
       | A    | 3%       | 1000 GBP         |
       | B    | 6%       | 1000 GBP         |
-    And "investor 1" invest 1000 GBP on tranche A on 03/10/2015
-    And "investor 3" invest 500 GBP on tranche B on 10/10/2015
-    When the interest calculation is done on 01/11/2015 for the period of 01/10/2015 to 31/10/2015
-    Then "investor 1" earns 28.06 GBP
-    And "investor 2" earns 21.29 GBP
+    And "investor 1" invest "1000 GBP" on tranche A on "03/10/2015"
+    And "investor 3" invest "500 GBP" on tranche B on "10/10/2015"
+    When the interest is calculated for the period of "01/10/2015" to "31/10/2015"
+    Then "investor 1" earns "28.06 GBP"
+    And "investor 2" earns "21.29 GBP"
